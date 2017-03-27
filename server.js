d268f4302db15f294e639c0a7c7cd7d1720990e4
F Grayson Meindl <meindlgrayson@gmail.com>
2017-03-27 15:44:20 -0700

    and more

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.static('./public'));
 
 app.get('/', function(request, response){
-  response.sendFile('./index.html', {root: './public'});
+  response.sendFile('/public/index.html', {root: '.'});
 });
 
 app.listen(PORT, function() {
