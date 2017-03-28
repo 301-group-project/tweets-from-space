5f721f30f59ecc60cc7d67a394a6534d4a85f940
Jane Smith <example@email.com>
2017-03-28 14:45:50 -0700

    working on filtering tweets by coordinate

--- public/scripts/model/tweets.js
+++ public/scripts/model/tweets.js
@@ -15,8 +15,22 @@ tweets.requestTweets = function() {
         tweets.all.push(ele);
       })
     })
-      .catch((err) => { console.error })
+      .catch(console.error);
 }
+tweets.requestTweets();
+
+// Model method that filters the data for tweets with a particular attribute.
+// we want to filter out all tweets that have null value for 'place'.
+//where do we pass in place as attribute?...lab code has it in about controller
+tweets.with = attr =>tweets.all.filter(tweet => tweet[attr]);
+
+
+  // tweets.all[0].place.bounding_box.coordinates[0][0] is what we want to keep
+ tweets.with(tweets.all[0].place.bounding_box.coordinates[0][0]);
+ console.log('we are here');
+
+
+
 
 module.tweets = tweets;
 })(window);
