4c58bb2092dc49430e68fed8ef96c07f82691fc9
Jane Smith <example@email.com>
2017-03-29 10:41:31 -0700

    started route for /map*

--- server.js
+++ server.js
@@ -12,6 +12,26 @@ app.get('/', function (request, response) {
   response.sendFile('/public/index.html', { root: '.' });
 });
 
+app.get('/map*',function(request, response) {
+  response.sendFile('/public/index.html', { root: '.' });
+  // var client = new Twitter({
+  //   consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  //   consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  //   access_token_key: process.env.TWITTER_ACCESS_TOKEN,
+  //   access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+  // });
+
+  // var params = { screen_name: req.query.screen_name || 'twts_frm_spce' };
+  // client.get('statuses/user_timeline', params, function (error, tweets) {
+  //   if (!error) {
+  //     console.log(req.query.screen_name);
+  //     res.send(tweets);
+  //   } else {
+  //     res.send(error);
+  //   }
+  // });
+});
+
 app.get('/tweets', function (req, res) {
   console.log('hello world');
   var client = new Twitter({
